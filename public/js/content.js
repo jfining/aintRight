febe912fc41bc5ae368366ac5eb6f864704163ca
Brian Shepard <surrealcanine@gmail.com>
2017-11-01 20:28:17 -0400

    Create intro content and some slides for 1-1

--- public/js/content.js
+++ public/js/content.js
@@ -1,27 +1,73 @@
 AintRight.contentData = {
 	intro: {
 		1: {
-			slots: [
-				{
-					init: "Here you go",
-					correct: "Whoops"
-				},
-				{
-					init: "What",
-					correct: "How?"
-				}
-			], answers: ["Whoops", "Where", "When", "How?", "Boy"]
+			question: "<p>Welcome to That Ain&#39;t Right!<br>" +
+			"Before we begin your training, let's explain how<br>" +
+			"the program works. Click the Next button to continue.</p>",
+			slots: [],
+			answers: []
 		}, 2: {
+			question: "<p>When white text boxes appear on a page,<br>" +
+			"drag answers into them from the gray text boxes below<br>" +
+			"the question. Drag &quot;What&quot; and &quot;How?&quot; into the<br>" +
+			"two text slots here.</p>" +
+			'<div id="slot0" class="text-slot answer-slot"></div>' +
+			'<div id="slot1" class="text-slot answer-slot"></div>',
             slots: [
                 {
-                    init: "One",
-                    correct: "A"
+                    init: "Text",
+                    correct: "What"
                 },
                 {
-                    init: "Two",
-                    correct: "B"
+                    init: "Box",
+                    correct: "How?"
                 }
-            ], answers: ["A", "B", "C", "D"]
-        }
+            ], answers: ["Gray", "What", "Mister", "How?"]
+        }, 3:{
+			question: "<p>The circles at the top mark your progress through a<br>" + 
+			"chapter. A white circle is a page with only text, like this one.<br>" +
+			"A gray circle is an unanswered question, and a green circle is a<br>" +
+			"question that's been answered correctly. The white-bordered circle<br>" +
+			"is the page you're on now. Just click a page to jump there instantly.</p>",
+			slots: [],
+			answers: []
+		}, 4:{
+			question: "<p>To select a chapter, click &quot;Chapters&quote to open the sidebar<br>" +
+			"and click the chapter you want to go to.</p>",
+			slots: [],
+			answers: []
+		}, 5:{
+			question: "<h2>Congratulations!</h2>" +
+			"<h3>You cleared the intro!</h3>" +
+			"<p>It took you <span id='timer'></span></p>" +
+			"<p>You answered <span id='score'></span> of 1 questions</p>",
+			slots: [],
+			answers: []
+		}
+	},
+	"1-1": {
+		1: {
+			question: "<p>A variable is like a box that holds information." +
+			"They have names and are assigned using this syntax:</p>" +
+			"<p class='code'>name = &quot;Brian&quot;<br>" + 
+			"answer = 7 * 6</p>",
+			slots: [],
+			answers: []
+		}, 2:{
+			question: "<p>Variables only exist once they are given a value.<br>" +
+			"Python doesn't like it when you try to use a variable that<br>" +
+			"doesn't exist. Make sure you spell your variables consistently!</p>" +
+			'<p class="code">camelot = "A rather silly place"<br>' +
+			'print(<div id="slot0" class="text-slot answer-slot"></div>)</p>',
+			slots: [{
+				init: "camelort",
+				correct: "camelot"
+			}],
+			answers: ["camelot"]
+		}, 3:{
+			question: "<p>To be continued</p>",
+			slots: [],
+			answers: []
+		}
 	}
 };
