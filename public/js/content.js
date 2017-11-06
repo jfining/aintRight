424d8eb70b6937180dfa9ca9f2a2b574e39a62cb
Brian Shepard <surrealcanine@gmail.com>
2017-11-06 09:51:58 -0500

    Add more content for 1-1 and 1-2

--- public/js/content.js
+++ public/js/content.js
@@ -24,7 +24,7 @@ const contentData = {
             answers: [],
             choices: []
         }, 4: {
-            question: "<p>To select a chapter, click &quot;Chapters&quote to open the sidebar<br>" +
+            question: "<p>To select a chapter, click &quot;Chapters&quot; to open the sidebar<br>" +
             "and click the chapter you want to go to.</p>",
             answers: [],
             choices: []
@@ -54,11 +54,82 @@ const contentData = {
             answers: ["camelot"],
             choices: ["camelot"]
         }, 3: {
-            question: "<p>To be continued</p>",
+            question: "<p>Variables can be modified with syntax like:</p>" +
+			"<p class='code'>spam = 2</br>spam = spam + 5 //spam now equals 2 + 5, or 7</p>" +
+			"<p>This can be shortened with some special syntax:</p>" +
+			"<p class='code'>spam = 2</br>spam += 5</p>",
             answers: [],
             choices: []
-        }
-    }
+        }, 4: {
+			question: "<p>Strings can be indexed or sliced to get parts of the string.<br>Indices start from 0.</p>" +
+			"<p class='code'>pet = 'dead parrot'<br>" +
+			"pet[0] // 'd'<br> pet[0:6] // 'dead p' (position 6 is excluded)<br>" +
+			"pet[-1] // 't' (first character from the end)<br>" +
+			"pet[:6] // 'dead p' (same as [0:6])<br>pet[6:] 'arrot' (6 to the end)</p>",
+            answers: [],
+            choices: []
+		}, 5: {
+			question: "<p>Strings can be overwritten by new strings, but not directly changed.</p>" +
+			"<p class='code'>weight = 'same as a duck'<br>" +
+			"weight[-4] = 'b' //Gives a TypeError<br>" +
+			"weight = 'same as a buck' //This works because it's a new string</p>",
+			answers: [],
+			choices: []
+		}, 
+		6: {
+			question: "<p>String indices equal or greater than the length of the string cause errors.<br>" +
+			"Negative string indices less than the negative length of the string cause errors.</p>" +
+			'<p class="code">foo = "spam"<br>foo[3] // "m" (remember, indices start from 0, not 1!)<br>' +
+			'foo[4] //Gives an IndexError<br>foo[-4] //"s"<br>foo[-5] //Gives an IndexError</p>',
+			answers: [],
+			choices: []
+		},7: {
+			question: "<h2>Congratulations!</h2>" +
+            "<h3>You cleared Chapter 1-1!</h3>" +
+            "<p>It took you <span id='timer'></span></p>" +
+            "<p>You answered <span id='score'></span> of 1 questions</p>",
+            answers: [],
+            choices: []
+		}
+    }, 3: {
+		1: {
+			question: "<p>What needs to change for foo to equal 8?</p>" +
+			'<p class="code">foo = 6</br>foo <div data-answer-index=0 id="slot0" class="text-slot answer-slot">+=</div> <div data-answer-index=1 id="slot1" class="text-slot answer-slot">foo + 2</div></p>',
+			answers: ["+=", "2"],
+			choices: ["+", "-=", "2", "foo + 8", "foo - 2"]
+		}, 2: {
+			question: "<p>Arrange the lines so bar = 24.</p>" +
+			'<p class="code">bar = 7<br>' +
+			'<div data-answer-index=0 id="slot0" class="text-slot answer-slot">bar += 8</div><br>' +
+			'<div data-answer-index=1 id="slot1" class="text-slot answer-slot">bar -= 3</div><br>' +
+			'<div data-answer-index=2 id="slot2" class="text-slot answer-slot">bar *= 4</div></p>',
+			answers: ["bar -= 3", "bar *= 4", "bar += 8"],
+			choices: []
+		}, 3: {
+			question: "<p>What result will the following code give?</p>" +
+			'<p class="code">ministry = "silly walks"<br>' +
+			'ministry[6] = "b"<br>' +
+			'print(ministry)</p>' +
+			'<p><div data-answer-index=0 id="slot0" class="text-slot answer-slot"></div></p>',
+			answers: ["TypeError on line 2"],
+			choices: ["sillybwalks", "silly balks", "TypeError on line 2"]
+		}, 4: {
+			question: "<p> What result will the following code give?</p>" +
+			'<p class="code">words = "out of cheese"<br>' +
+			'more_words = words[0:2] + words[-6:]<br>' +
+			'print(more_words)</p>' +
+			'<div data-answer-index=0 id="slot0" class="text-slot answer-slot"></div>',
+			answers: ["oucheese"],
+			choices: ["outcheese", "IndexError on line 2", "oucheese", "out cheese"]
+		}, 5: {
+			question: "<h2>Congratulations!</h2>" +
+            "<h3>You cleared Chapter 1-2!</h3>" +
+            "<p>It took you <span id='timer'></span></p>" +
+            "<p>You answered <span id='score'></span> of 4 questions</p>",
+            answers: [],
+            choices: []
+		}
+	}
 };
 
 try {
